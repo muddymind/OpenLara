43c0f6a7ecfd7d6cca37db6d4898f1cceef7a4ae
headshot2017 <nou@jm>
2022-08-20 22:56:22 -0400

    alter underwater mario shader
    grabbed code from mario.glsl and pasted it into waterMario.glsl

--- src/shaders/waterMario.glsl
+++ src/shaders/waterMario.glsl
@@ -14,8 +14,11 @@ uniform sampler2D marioTex;
 v2f vec3 v_position;
 v2f vec3 v_color;
 v2f vec3 v_normal;
-v2f vec3 v_light;
+v2f vec4 v_light;
 v2f vec2 v_uv;
+v2f vec4 vViewVec;	// xyz - dir * dist, w - coord.y * clipPlaneSign
+v2f vec4 vDiffuse;
+v2f vec3 vLightVec;
 
 #ifdef VERTEX
 
@@ -29,8 +32,32 @@ v2f vec2 v_uv;
 		v_position = position;
 		v_color = color;
 		v_normal = normal;
-		v_light = transpose( mat3( view )) * normalize( vec3( 1 ));
 		v_uv = uv;
+
+		// code from compose.glsl
+		vViewVec = vec4((uViewPos.xyz - position) * 1, 0.0);
+		vDiffuse = vec4(v_color * uMaterial.x, 1.0);
+		vDiffuse.xyz *= 2.0;
+		vDiffuse *= uMaterial.w;
+
+		vec3 lv0 = (uLightPos[0].xyz - position) * uLightColor[0].w;
+		vec3 lv1 = (uLightPos[1].xyz - position) * uLightColor[1].w;
+		vec3 lv2 = (uLightPos[2].xyz - position) * uLightColor[2].w;
+		vec3 lv3 = (uLightPos[3].xyz - position) * uLightColor[3].w;
+		vLightVec = lv0;
+
+		vec4 lum, att;
+		lum.x = dot(normal, normalize(lv0)); att.x = dot(lv0, lv0);
+		lum.y = dot(normal, normalize(lv1)); att.y = dot(lv1, lv1);
+		lum.z = dot(normal, normalize(lv2)); att.z = dot(lv2, lv2);
+		lum.w = dot(normal, normalize(lv3)); att.w = dot(lv3, lv3);
+		vec4 light = max(vec4(0.0), lum) * max(vec4(0.0), vec4(1.0) - att);
+
+		vec3 ambient = vec3(uMaterial.y);
+
+		v_light.xyz = uLightColor[1].xyz * light.y + uLightColor[2].xyz * light.z + uLightColor[3].xyz * light.w;
+		v_light.w = 0.0;
+		v_light.xyz += ambient + uLightColor[0].xyz * light.x;
 
 		gl_Position = projection * view * vec4( position, 1. );
 	}
@@ -42,8 +69,9 @@ v2f vec2 v_uv;
 
 	void main()
 	{
-		float light = .5 + .5 * clamp( dot( v_normal, v_light ), 0., 1. );
 		vec4 texColor = texture2D( marioTex, v_uv );
+		vec3 light = v_light.xyz;
+
 		vec3 mainColor = mix( v_color, texColor.rgb, texColor.a ); // v_uv.x >= 0. ? texColor.a : 0. );
 		color = vec4( mainColor * light, 1 );
 
